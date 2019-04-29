477137efd55f051c077174bef6cceee3c31288ad
Ruby M <ranmi.chaos.fox@gmail.com>
2019-04-29 14:35:14 -0400

    Sewing tetrahedra into star, now have to wrap around to 4-simplex

--- making_simplices.cpp
+++ making_simplices.cpp
@@ -23,17 +23,17 @@ typedef K::Affinely_independent_d independent;
 typedef std::vector<Point> vector;
 
 typedef CGAL::Combinatorial_map<dim> CMap;
-typedef CMap::Dart_const_handle Dart_const_handle; 
+typedef CMap::Dart_handle Dart_handle; 
 
 // Get rid of this forward declaration later and use header files, pls
-Dart_const_handle make_simplex(int simplex_dimension, CMap cm);
+Dart_handle make_simplex(int simplex_dimension, CMap cm);
 
 int main(){
     CMap cm;
     make_simplex(4,cm);
 }
 
-Dart_const_handle make_simplex(int simplex_dimension, CMap cm){
+Dart_handle make_simplex(int simplex_dimension, CMap cm){
     if (simplex_dimension == 2){
         return cm.make_combinatorial_polygon(3);
     }
@@ -41,14 +41,25 @@ Dart_const_handle make_simplex(int simplex_dimension, CMap cm){
         return cm.make_combinatorial_tetrahedron();
     }
     if (simplex_dimension == 4){
-        std::vector<Dart_const_handle> components;
-        for(int i=0; i<simplex_dimension+1; i++){
-            components.push_back(make_simplex(simplex_dimension-1, cm));
-        }
-        cm.sew<3>(components[1], components[2]);
-        cm.sew<3>(cm.beta(components[1], 2), components[3]);
-        cm.sew<3>(cm.beta(cm.beta(components[1], 1), 2), components[4]);
-        cm.sew<3>(cm.beta(cm.beta(components[1], 0), 2), components[5]);
+        std::vector<Dart_handle> components;
+        components.push_back(cm.make_combinatorial_tetrahedron());
+        components.push_back(cm.make_combinatorial_tetrahedron());
+        components.push_back(cm.make_combinatorial_tetrahedron());
+        components.push_back(cm.make_combinatorial_tetrahedron());
+        components.push_back(cm.make_combinatorial_tetrahedron());
+
+        // for(int i=0; i<simplex_dimension+1; i++){
+        //     components.push_back(make_simplex(simplex_dimension-1, cm));
+        // }
+        std::cout << "test1" << std::endl;
+        cm.sew<3>(components[0], components[1]);
+        std::cout << "test2" << std::endl;
+        cm.sew<3>(cm.beta(components[0], 2), components[2]);
+        std::cout << "test3" << std::endl;
+        cm.sew<3>(cm.beta(cm.beta(components[0], 1), 2), components[3]);
+        std::cout << "test4" << std::endl;
+        cm.sew<3>(cm.beta(cm.beta(components[0], 0), 2), components[4]);
+        std::cout << "test5" << std::endl;
 
         CGAL_assertion(cm.is_valid());
         
@@ -60,7 +71,7 @@ Dart_const_handle make_simplex(int simplex_dimension, CMap cm){
         
     //     // Make d+1 smaller dimensional simplices
 
-    //     std::vector<Dart_const_handle> components;
+    //     std::vector<Dart_handle> components;
 
     //     for(int i=0; i<=simplex_dimension; i++){
     //         components.push_back(make_simplex(simplex_dimension-1, cm));
